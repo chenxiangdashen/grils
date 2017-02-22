b611944d751f915828005c9cbcd36079e03d8545
chenxiangdashen <chenxiangdashen123@163.com>
2017-02-22 14:20:55 +0800

    fisrt

---
+++ src/main/java/com/cx/controller/simpleController.java
@@ -0,0 +1,13 @@
+package com.cx.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by Administrator on 2017/2/21.
+ */
+
+
+public class simpleController {
+
+}
